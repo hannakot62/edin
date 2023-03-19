39f3242636aca3497b7cc0d42f3437c6f2b2f93e
HANNAKOT62 <2kotyagova6@gmail.com>
2023-03-19 12:32:33 +0300

    favicon

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import icon from './assets/icon.png'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+todoIcon.href = icon
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
